1a310c52e83b12c2d80305c8a50678f743914432
GL3MON <harisshragavarchives@gmail.com>
2026-03-25 13:00:44 +0530

    workflow changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,29 @@ variable "gateway_url" {}
 variable "username" {}
 variable "password" {}
 
-resource "openfaas_function" "test" {
-  name  = "test-fn"
-  image = "gl3mon/test-fn:latest"
+resource "null_resource" "deploy" {
+  provisioner "local-exec" {
+    command = <<EOT
+echo "${var.password}" | faas-cli login \
+  --username ${var.username} \
+  --password-stdin \
+  --gateway ${var.gateway_url}
+
+faas-cli deploy -f stack.yml --gateway ${var.gateway_url}
+EOT
+  }
+}
+
+resource "null_resource" "destroy" {
+  provisioner "local-exec" {
+    when    = destroy
+    command = <<EOT
+echo "${var.password}" | faas-cli login \
+  --username ${var.username} \
+  --password-stdin \
+  --gateway ${var.gateway_url}
+
+faas-cli remove test-fn --gateway ${var.gateway_url}
+EOT
+  }
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,14 +1,7 @@
 terraform {
   required_providers {
-    openfaas = {
-      source  = "openfaas/openfaas"
-      version = ">= 0.1.0"
+    null = {
+      source = "hashicorp/null"
     }
   }
-}
-
-provider "openfaas" {
-  uri      = var.gateway_url
-  username = var.username
-  password = var.password
 }
