7039960b1c8841ff34908ea75724a6dd59513ca2
GL3MON <harisshragavarchives@gmail.com>
2026-03-25 12:56:12 +0530

    workflow changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,6 @@
-terraform {
-  required_providers {
-    openfaas = {
-      source  = "openfaas/openfaas"
-      version = ">= 0.1.0"
-    }
-  }
-}
-
 variable "gateway_url" {}
 variable "username" {}
 variable "password" {}
-
-provider "openfaas" {
-  uri      = var.gateway_url
-  username = var.username
-  password = var.password
-}
 
 resource "openfaas_function" "test" {
   name  = "test-fn"
