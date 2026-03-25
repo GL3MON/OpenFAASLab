c5326d092618aea05ea7707537dc2d09f75fb487
GL3MON <harisshragavarchives@gmail.com>
2026-03-25 12:45:11 +0530

    added openfaas cicd workflows

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
-terraform {
-  required_providers {
-    openfaas = {
-      source  = "openfaas/openfaas"
-      version = ">= 0.1.0"
-    }
-  }
-}
+variable "gateway_url" {}
+variable "username" {}
+variable "password" {}
 
 provider "openfaas" {
   uri      = var.gateway_url
   username = var.username
   password = var.password
+}
+
+resource "openfaas_function" "fn" {
+  name  = "test-fn"
+  image = "gl3mon/test-fn:latest"
 }
